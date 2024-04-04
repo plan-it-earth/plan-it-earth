e85b1bcc940d146f76d15f9a5fdbd715d8f371cc
Enrique Romero <enrique.romero1110@gmail.com>
2024-04-03 21:38:39 -0400

    Wrote storeEvents, working on fetching

--- app/pages/calendar/page.jsx
+++ app/pages/calendar/page.jsx
@@ -1,8 +1,8 @@
 'use client';
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 import { onAuthStateChanged } from 'firebase/auth';
-import { auth } from '../../../firebaseConfig';
+import { auth, db } from '../../../firebaseConfig';
 import { useRouter} from 'next/navigation';
 
 import Header from '../../Components/Header';
@@ -11,12 +11,15 @@ import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
-import iCalendarPlugin from '@fullcalendar/icalendar'
+import iCalendarPlugin from '@fullcalendar/icalendar';
 
 import '../../Styles/calendar.css';
+import { doc, setDoc, getDoc } from 'firebase/firestore';
 
 export default function Calendar() {
     const router = useRouter();
+    const calendarRef = useRef(null);
+    let uid = null;
 
     useEffect(() => {
         // if user is not logged in, redirect to login page
@@ -34,12 +37,55 @@ export default function Calendar() {
         // still needs to pass date to new page
     }
 
+    const storeEvents = async () => {
+        const calendarApi = calendarRef.current.getApi()
+        const eventArray = calendarApi.getEvents();
+
+        if(!eventArray) {
+            console.log('no events to store');
+            return;
+        }
+
+        const eventJson = JSON.stringify(eventArray);
+
+        onAuthStateChanged(auth, async (user) => {
+            if (user) {
+              // User is signed in
+              const uid = user.uid;
+              
+              await setDoc(doc(db, "users", uid), {
+                events: eventJson
+            });
+            }
+        });
+    }
+
+    const fetchEvents = async () => {
+        let events = null;
+        onAuthStateChanged(auth, async (user) => {
+            if (user) {
+                // User is signed in
+                const uid = user.uid;
+                const docRef = doc(db, "users", uid);
+                const docSnap = await getDoc(docRef);
+                if (docSnap.exists()) {
+                    events = docSnap.data().events;
+                } else {
+                    // doc.data() will be undefined in this case
+                    console.log("No such document!");
+                }
+            }
+        });
+        return events;
+    }
+
     return (
         <div>
             <Header />
             <div id="portal-root"></div>
             <main className="mt-12">
                 <FullCalendar
+                    ref={calendarRef}
                     plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, iCalendarPlugin]}
                     headerToolbar={{
                         left: 'prev,next today',
@@ -58,6 +104,12 @@ export default function Calendar() {
                             url: '/US_en.ics',
                             format: 'ics',
                             failure: function() {console.log('failed to fetch events')},
+                        }
+                    }
+                    eventSources={
+                        {
+                            url: fetchEvents(),
+                            failure: function() {console.log('failed to fetch events from DB')},
                         }
                     }
                 />
