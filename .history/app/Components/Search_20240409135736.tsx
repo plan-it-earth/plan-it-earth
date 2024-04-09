05fe41ca9ba70810f8776bff37e23833c414bbb5
robbymcd <99295997+robbymcd@users.noreply.github.com>
2024-04-09 14:02:54 -0400

    fixed search bar

--- .history/app/Components/Search_20240409135736.tsx
+++ .history/app/Components/Search_20240409135736.tsx
@@ -6,7 +6,7 @@ import search from '../Images/search.png';
 
 export default function Search() {
     return (
-        <div className="flex flex-row h-10 w-fit items-center gap-2 self-center bg-[#fff] rounded-lg md:w-96 sm:w-80 md:gap-4 md:ml-16">
+        <div className="flex flex-row h-10 w-fit items-center gap-2 self-center ml-5 bg-[#fff] rounded-lg md:w-96 sm:w-80 md:gap-4">
             <Image src={search} alt='Search' height={18} width={18} className="h-4 w-4 ml-2 cursor-pointer md:ml-3"/>
             <input type='text' placeholder="Search your notes" className="w-full text-black placeholder-gray-600 text-xs outline-none border-none md:text-sm"/>
         </div>
