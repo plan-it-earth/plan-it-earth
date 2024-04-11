06b7fb72216dcc93500475dbb2d7a997a67da21c
georgiewald <georgiewald@yahoo.com>
2024-04-11 17:31:34 -0400

    Fixed the links in exportorshare

--- app/pages/exportorshare/page.tsx
+++ app/pages/exportorshare/page.tsx
@@ -5,52 +5,34 @@ import Pagewhite from '../../Images/pagewhite.png';
 import Image from 'next/image';
 import Sharewhite from '../../Images/sharewhite.png';
 import '../../globals.css';
-import { useRouter } from 'next/navigation';
-import ViewShared from '../../Images/viewshared.png';
+import ViewShared from '../../Images/viewshared.png'
 
 export default function Home() {
-
-    const router = useRouter();
-
-    const routeToShare = () => {
-        router.push('./exportorshare/sharewith')
-    }
-
-    const routeToExport = () => {
-        router.push('./exportorshare/export')
-    }
-
-    const routeToViewShared = () => {
-        router.push('./exportorshare/viewshared')
-    }
-
     return (
-        
         <div className="bg-[#16141C] min-h-screen">
-          <Header />
-          <Image src={Exportorshareicon} alt="export share logo" height={100} width={100} className="mx-auto mb-6 mt-16"/>
-          <h2 className="flex flex-col items-center justify-center text-2xl font-semibold mb-6 mt-16">Export or Share</h2>
-          <div className="flex justify-center">
-            <div className="max-w-md mr-8 p-8 bg-[#1A1926] rounded-lg shadow-md">
-              <button onClick={routeToShare} className="flex flex-col items-center justify-center buttonexportpage">
-                <Image src={Sharewhite} alt="white share arrow" height={100} width={100} />
-                <span>Share</span>
-              </button>
+            <Header />
+            <Image src={Exportorshareicon} alt="export share logo" height={100} width={100} className="mx-auto mb-6 mt-16" />
+            <h2 className="flex flex-col items-center justify-center text-2xl font-semibold mb-6 mt-16">Export or Share</h2>
+            <div className="flex justify-center">
+                <div className="max-w-md mr-8 p-8 bg-[#1A1926] rounded-lg shadow-md">
+                    <a href="./exportorshare/sharewith" className="flex flex-col items-center justify-center buttonexportpage">
+                        <Image src={Sharewhite} alt="white share arrow" height={100} width={100} />
+                        <span>Share</span>
+                    </a>
+                </div>
+                <div className="max-w-md mr-8 p-8 bg-[#1A1926] rounded-lg shadow-md">
+                    <a href="./exportorshare/export" className="flex flex-col items-center justify-center buttonexportpage">
+                        <Image src={Pagewhite} alt="white page icon" height={100} width={100} />
+                        <span>Export as pdf</span>
+                    </a>
+                </div>
+                <div className="max-w-md p-8 bg-[#1A1926] rounded-lg shadow-md">
+                    <a href="./exportorshare/viewshared" className="flex flex-col items-center justify-center buttonexportpage">
+                        <Image src={ViewShared} alt="view shared" height={100} width={100} />
+                        <span>View Shared</span>
+                    </a>
+                </div>
             </div>
-            <div className="max-w-md mr-8 p-8 bg-[#1A1926] rounded-lg shadow-md">
-              <button onClick={routeToExport} className="flex flex-col items-center justify-center buttonexportpage">
-                <Image src={Pagewhite} alt="white page icon" height={100} width={100} />
-                <span>Export as pdf</span>
-              </button>
-            </div>
-            {/* New Box */}
-            <div className="max-w-md p-8 bg-[#1A1926] rounded-lg shadow-md">
-              <button onClick={routeToViewShared} className="flex flex-col items-center justify-center buttonexportpage">
-                <Image src={ViewShared} alt="view shared" height={100} width={100} />
-                <span>View Shared</span>
-              </button>
-            </div>
-          </div>
         </div>
-    )
+    );
 }
