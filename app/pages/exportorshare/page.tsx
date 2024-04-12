bec0f2179de6586fdeea5e01ace3f1142f2bfbc9
georgiewald <georgiewald@yahoo.com>
2024-04-12 17:20:17 -0400

    changed exportorshare from tags to links

--- app/pages/exportorshare/page.tsx
+++ app/pages/exportorshare/page.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 import Sharewhite from '../../Images/sharewhite.png';
 import '../../globals.css';
 import ViewShared from '../../Images/viewshared.png'
+import Link from 'next/link';
 
 export default function Home() {
     return (
@@ -15,22 +16,22 @@ export default function Home() {
             <h2 className="flex flex-col items-center justify-center text-2xl font-semibold mb-6 mt-16">Export or Share</h2>
             <div className="flex justify-center">
                 <div className="max-w-md mr-8 p-8 bg-[#1A1926] rounded-lg shadow-md">
-                    <a href="./exportorshare/sharewith" className="flex flex-col items-center justify-center buttonexportpage">
+                    <Link className="flex flex-col items-center justify-center buttonexportpage" href={{pathname: './exportorshare/sharewith'}}>
                         <Image src={Sharewhite} alt="white share arrow" height={100} width={100} />
                         <span>Share</span>
-                    </a>
+                    </Link>
                 </div>
                 <div className="max-w-md mr-8 p-8 bg-[#1A1926] rounded-lg shadow-md">
-                    <a href="./exportorshare/export" className="flex flex-col items-center justify-center buttonexportpage">
+                    <Link className="flex flex-col items-center justify-center buttonexportpage" href={{pathname: './exportorshare/export'}}>
                         <Image src={Pagewhite} alt="white page icon" height={100} width={100} />
                         <span>Export as pdf</span>
-                    </a>
+                    </Link>
                 </div>
                 <div className="max-w-md p-8 bg-[#1A1926] rounded-lg shadow-md">
-                    <a href="./exportorshare/viewshared" className="flex flex-col items-center justify-center buttonexportpage">
+                    <Link className="flex flex-col items-center justify-center buttonexportpage" href={{pathname: './exportorshare/viewshared'}}>
                         <Image src={ViewShared} alt="view shared" height={100} width={100} />
                         <span>View Shared</span>
-                    </a>
+                    </Link>
                 </div>
             </div>
         </div>
