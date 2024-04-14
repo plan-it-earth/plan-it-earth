a0bdc41a086707e8fa1120568df03692109f781a
robbymcd <99295997+robbymcd@users.noreply.github.com>
2024-04-14 15:54:23 -0400

    add custom colors per events groupId

--- .history/app/layout_20240414155254.tsx
+++ .history/app/layout_20240414155254.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import AuthProvider from "./lib/firebase/AuthProvider";
+import { AuthProvider } from "./lib/firebase/AuthProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="#bg-[#16141C]">
       <AuthProvider>
         <body className={inter.className}>{children}</body>
       </AuthProvider>
