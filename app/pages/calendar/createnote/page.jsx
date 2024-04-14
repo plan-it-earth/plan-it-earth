c479dbafe98777d0cda4bb3e482d38cf114b5d79
Enrique Romero <enrique.romero1110@gmail.com>
2024-04-14 19:39:34 -0400

    simple comment

--- app/pages/calendar/createnote/page.jsx
+++ app/pages/calendar/createnote/page.jsx
@@ -82,6 +82,7 @@ export default function CreateNote() {
                 },
             });
         } else {
+            // All day event
             let start = new Date(date.value);
 
             calendarApi.addEvent({
