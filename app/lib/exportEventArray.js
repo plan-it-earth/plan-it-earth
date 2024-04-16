3c80f75f6b503894132c898eedd61bc24892c615
Enrique Romero <enrique.romero1110@gmail.com>
2024-04-16 18:22:46 -0400

    Progress, just need images

--- app/lib/exportEventArray.js
+++ app/lib/exportEventArray.js
@@ -19,10 +19,80 @@ event {
 
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
-export const exportEventArray = (events) => {
+export const exportEventArray = async (eventsArray) => {
     let docDefinition = {
-        
-    }
+        content: [],
+        styles: {
+            title: {
+                fontSize: 20,
+                bold: true,
+                margin: [0, 0, 0, 10]
+            },
+            date: {
+                fontSize: 14,
+                margin: [0, 0, 0, 10]
+            },
+            image: {
+                margin: [0, 0, 0, 10]
+            },
+            description: {
+                margin: [0, 0, 0, 10]
+            },
+            labelAndAlarm: {
+                margin: [0, 0, 0, 10]
+            }
+        }
+    };
+
+    eventsArray.forEach(async event => {
+        // Title
+        docDefinition.content.push({ text: event.title, style: 'title' });
+
+        // Date and time or all day label
+        const dateTime = event.start.toLocaleString();
+        const allDay = event.allDay === true;
+        const dateLabel = allDay ? 'All Day' : dateTime;
+        docDefinition.content.push({ text: dateLabel, style: 'date' });
+
+        // Image (if available)
+        if (event.extendedProps.image !== '' && event.extendedProps.image !== undefined) {
+            // Convert image url to base64 string
+            const addImageToDoc = async () => {
+                const response = await fetch(event.extendedProps.image);
+                const blob = await response.blob();
+                return new Promise((resolve, reject) => {
+                    const reader = new FileReader();
+                    reader.readAsDataURL(blob);
+                    reader.onloadend = () => {
+                        const base64String = reader.result;
+                        console.log(base64String);
+                        docDefinition.content.push({ image: base64String, style: 'image' });
+                        console.log('Image added to doc');
+                        resolve();
+                    };
+                    reader.onerror = reject;
+                });
+            };
+            await addImageToDoc();
+        }
+        console.log('Past image check')
+
+        // Description (if available)
+        if (event.extendedProps.description) {
+            docDefinition.content.push({ text: event.extendedProps.description, style: 'description' });
+        }
+
+        // Label (or blank if label is userEvent) followed by alarm (if available)
+        const label = event.groupId === 'userEvent' ? '' : event.groupId;
+        const alarm = event.extendedProps.alarm;
+        const labelAndAlarm = label + (alarm >= 0 ? ` (Alarm: ${alarm} minutes)` : '');
+        docDefinition.content.push({ text: labelAndAlarm, style: 'labelAndAlarm' });
+
+        // Space to make room for the next event
+        docDefinition.content.push({ text: '\n\n' });
+    });
+
+    console.log(docDefinition);
 
     pdfMake.createPdf(docDefinition).open();
 }
