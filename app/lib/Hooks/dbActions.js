aa618c26c7d8d9c0db4d8685412136f1157a4033
Enrique Romero <enrique.romero1110@gmail.com>
2024-04-17 18:55:40 -0400

    Update event documentation

--- app/lib/Hooks/dbActions.js
+++ app/lib/Hooks/dbActions.js
@@ -27,14 +27,14 @@ export const storeEvents = async (userData, calendarApi) => {
 
 // Returns an array of the users events using the Event object structure
 // event {
-//     id: calendarApi.getEvents().length + 1,     // Unique Identifier, doesnt need to be printed
-//     title: title.value,     // Title of the event
-//     start: start,           // Date and time of the event given in Javascripts Date() object
-//     groupId: label.value,   // A label of the type of activity it is
+//     id               // Unique Identifier, doesnt need to be printed
+//     title            // Title of the event
+//     start            // Date and time of the event given in Javascripts Date() object
+//     groupId          // A label of the type of activity it is
 //     extendedProps: {
-//         alarm: alarm.value, // Value of how many minutes before an event a user has an alarm for, -1 if no alarm
-//         image: imageUrl,    // May contain a URL of user submitted image - blank if no image
-//         description: description.value  // May contain description of notes - blank if no description
+//         alarm        // Value of how many minutes before an event a user has an alarm for, -1 if no alarm
+//         image        // May contain a URL of user submitted image - blank if no image
+//         description  // May contain description of notes - blank if no description
 //     },
 // }
 export const fetchEvents = async (userData) => {
