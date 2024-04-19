57dc9b9f84652f3b7fd0347b3e76b7d7c6de4f3b
Enrique Romero <enrique.romero1110@gmail.com>
2024-04-19 18:44:13 -0400

    ALERTS ARE WORKING NOW

--- app/lib/checkAlarms.js
+++ app/lib/checkAlarms.js
@@ -1,21 +1,18 @@
 const getUpcomingEvents = (events) => {
-    console.log("Getting upcoming events");
-    console.log("Events: ", events);
     const now = new Date();
     now.setSeconds(0, 0);
-    console.log("Current time: ", now);
     
     // Filter out events that have an alarm set and are upcoming
     const upcomingEvents = events.filter((event) => {
-        const alarmTime = new Date(event.alarmTime);
-        console.log("Alarm time: ", alarmTime);
+        const alarmTime = new Date(event.extendedProps.alarmTime);
+        if(isNaN(alarmTime.getTime())) { return false; }
         return alarmTime.getTime() >= now.getTime() && alarmTime.getTime() < now.getTime() + 60000;
     });
 
     // Sort the upcoming events by start time
     upcomingEvents.sort((a, b) => {
-        const aAlertTime = new Date(a.alertTime);
-        const bAlertTime = new Date(b.alertTime);
+        const aAlertTime = new Date(a.extendedProps.alertTime);
+        const bAlertTime = new Date(b.extendedProps.alertTime);
         return aAlertTime - bAlertTime;
     });
 
@@ -23,16 +20,14 @@ const getUpcomingEvents = (events) => {
 }
 
 export const checkAlarms = (events) => {
-    console.log("Checking alarms");
     const upcomingEvents = getUpcomingEvents(events);
-    console.log("Upcoming events: ", upcomingEvents);
 
     // Alert for each event currently in the upcomingEvents array
-    for(currEvent in upcomingEvents) {
-        if(currEvent.alert === 0) {
+    for(let currEvent of upcomingEvents) {
+        if(currEvent.extendedProps.alarm === "0") {
             alert(currEvent.title + " is starting now!");
         } else {
-            alert(currEvent.title + " is starting in " + currEvent.alert + " minutes!");
+            alert(currEvent.title + " is starting in " + currEvent.extendedProps.alarm + " minutes!");
         }
     }
 }
